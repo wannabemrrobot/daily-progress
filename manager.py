a1a50e4cb4aef535423807cf3f85ee9b6e6f00d7
wannabemrrobot <dv.official.10010@gmail.com>
2021-08-09 15:51:00 +0530

    manger.py edit

--- manager.py
+++ manager.py
@@ -1,9 +1,32 @@
-def createTheme():
+#!/usr/bin/env python3
+from simple_term_menu import TerminalMenu
+#----------------------------------------
+def createTimeline():
+    print("timeline")
     pass
 
+def createPosts():
+    print("posts")
+    pass
+
+def createTheme():
+    print("theme")
+    pass
+
+def main():
+    print(">> Select function: ")
+    options = ["1. create timeline event", "2. create posts", "3. create theme"]
+    terminal_menu = TerminalMenu(options)
+    menu_entry_index = terminal_menu.show()
+    # print(f"You have selected {options[menu_entry_index]}")
+    if(menu_entry_index == 0):
+        createTimeline()
+    elif(menu_entry_index == 1):
+        createPosts()
+    elif(menu_entry_index == 2):
+        createTheme()
 
 
 
 if __name__ == "__main__":
-    
-    print(">> Select function: \n 1. create theme \n 2. create posts \n 3. create timeline event \n")
+    main()
